2822ce1083f73ef73f51f1e10119537adea73df0
ChienDuy <chienduy2020@gmail.com>
2022-09-04 20:21:50 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+window.addEventListener("scroll", () => {
+    let headerTop = document.querySelector(".header-top");
+    let headerLogo = document.querySelector(".header-menu-logo");
+    let headerLogoSm = document.querySelector(".header-menu-logo-sm");
+    let backToTop = document.querySelector(".back-to-top");
+
+    headerTop.classList.toggle("hide", window.scrollY > 0);
+    headerLogo.classList.toggle("hide", window.scrollY > 0);
+    headerLogoSm.classList.toggle("show", window.scrollY > 0);
+    backToTop.classList.toggle("show", window.scrollY > 0);
+})
+
+let heroShareBtn = document.querySelector(".hero-share-btn");
+let heroSocial = document.querySelector(".hero-share-social");
+
+heroShareBtn.addEventListener("click", () => {
+    heroSocial.classList.toggle("visible");
+})
